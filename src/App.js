e2d2ec2647169bb39621df58abf246db3d0c4b99
Anjali Raj <anjaliraj2024a@mca.ajce.in>
2024-07-19 11:17:52 +0530

    Elements and function

--- src/App.js
+++ src/App.js
@@ -2,22 +2,19 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const name='anjali';
+  const x=70;
+  const y=7;
+  const message=()=>{
+    return "Good MOrning"
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-         Hello world. I'm Anjali
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <h1 className="heading">Hello World</h1>
+     <h1>My name is {name}</h1>
+     <h2>Sum of 10 and 6 ={10+6}</h2>
+     <h2>Sum of x and y={x+y}</h2>
+     <h2>{message()}</h2>
     </div>
   );
 }
