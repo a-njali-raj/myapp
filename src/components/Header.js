757c1d4c15045202909dd4b4a4f227a74d1c1c23
Anjali Raj <anjaliraj2024a@mca.ajce.in>
2024-07-21 10:52:20 +0530

    css inline and variable

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,13 @@
 import React from 'react'
 
 const Header = () => {
+  const headingstyle={color:'blue'};
   return (
     <div>
-        <h1>component 1</h1>
+        <h1 style={{color:'red'}}>component 1</h1>
+        <h1 style={headingstyle}>component</h1>
     </div>
-  )
-}
+  );
+};
 
 export default Header
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,12 +1,12 @@
 import React from 'react'
-import Child from './Child';
+
 
 const Home = () => {
   return (
     <div>
         <h1>
             Home Component
-            <Child/>
+           
         </h1>
     </div>
   );
