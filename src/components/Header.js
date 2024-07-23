28f949af33ebf02c50cdea6e89076823cc30b596
Anjali Raj <anjaliraj2024a@mca.ajce.in>
2024-07-23 08:41:52 +0530

    Events

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,105 +1,35 @@
 import React from 'react'
 import {Button, Table, Accordion, Card, Navbar, Nav, Container, NavDropdown  } from 'react-bootstrap';
 const Header = () => {
-
+  const clickHandle=()=>
+  {
+    console.log("you clicked me");
+  };
+  const DbClickHandle=(name)=>
+  {
+    alert("Hello" + name);
+  };
+  const onChange=(name)=>
+    {
+      alert("you typed in textbox");
+    };
+    const onMouseOver=(name)=>
+      {
+        alert("you mouseover the elemnt");
+      };
+    const onKeyDown=()=>
+    {
+      alert("you pressed the key");
+    };
   return (
    <>
-   <Container>
-   <Navbar expand="lg" className="bg-body-tertiary">
-      <Container>
-        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#link">Link</Nav.Link>
-            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">
-                Another action
-              </NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">
-                Separated link
-              </NavDropdown.Item>
-            </NavDropdown>
-          </Nav>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
-   <h1>hello world</h1>
-   <Button variant="success">success</Button>{' '}
-   <Button variant="danger">danger</Button>{' '}
-   <br>
-   </br>
-   <Table striped bordered hover>
-      <thead>
-        <tr>
-          <th>#</th>
-          <th>First Name</th>
-          <th>Last Name</th>
-          <th>Username</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr>
-          <td>1</td>
-          <td>Mark</td>
-          <td>Otto</td>
-          <td>@mdo</td>
-        </tr>
-        <tr>
-          <td>2</td>
-          <td>Jacob</td>
-          <td>Thornton</td>
-          <td>@fat</td>
-        </tr>
-        <tr>
-          <td>3</td>
-          <td colSpan={2}>Larry the Bird</td>
-          <td>@twitter</td>
-        </tr>
-      </tbody>
-    </Table>
-    <Accordion defaultActiveKey="0">
-      <Accordion.Item eventKey="0">
-        <Accordion.Header>Accordion Item #1</Accordion.Header>
-        <Accordion.Body>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-          aliquip ex ea commodo consequat. Duis aute irure dolor in
-          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-          culpa qui officia deserunt mollit anim id est laborum.
-        </Accordion.Body>
-      </Accordion.Item>
-      <Accordion.Item eventKey="1">
-        <Accordion.Header>Accordion Item #2</Accordion.Header>
-        <Accordion.Body>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-          aliquip ex ea commodo consequat. Duis aute irure dolor in
-          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-          culpa qui officia deserunt mollit anim id est laborum.
-        </Accordion.Body>
-      </Accordion.Item>
-    </Accordion>
-    <Card style={{ width: '18rem' }}>
-      <Card.Img variant="top" src="https://picsum.photos/200/300" />
-      <Card.Body>
-        <Card.Title>Card Title</Card.Title>
-        <Card.Text>
-          Some quick example text to build on the card title and make up the
-          bulk of the card's content.
-        </Card.Text>
-        <Button variant="primary">Go somewhere</Button>
-      </Card.Body>
-    </Card>
-    </Container>
+   
+   <h1 className="bg-success text-white">Learn react events</h1>
+   <Button onClick={clickHandle}>Click Me</Button>
+   <h1 onDoubleClick={() => DbClickHandle("Anjali")}>Double click</h1>
+   <input onChange={onChange} type="text" />
+   <h1 onMouseOver={onMouseOver}>MouseOver</h1>
+   <input onKeyDown={onKeyDown} type="text"/>
    </>
   );
 };
