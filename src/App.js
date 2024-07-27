0ce84861c0b708bd55949a693b91f0beef54d617
Anjali Raj <anjaliraj2024a@mca.ajce.in>
2024-07-27 17:19:45 +0530

    using ternary operation condition using within jsx e.g.1

--- src/App.js
+++ src/App.js
@@ -1,26 +1,12 @@
 
 import './App.css';
 import React from 'react';
-import Header from './components/Header';
-import Home from'./components/Home';
-import States from './components/States';
-import { Container } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 function App() {
-  const Checknum = ({ number })=>{
-    if(number>0){
-      return <h1>positive number</h1>;
-    }
-    else if(number<0){
-      return<h1>negative number</h1>;
-    }
-    else{
-      return<h1>Zero</h1>;
-    }
-  }
+  let num=1;
   return(
  <div className='myapp'>
-  <Checknum number={-5}/>
+  {num>0?<h1>positive</h1>:num<0?<h1>negative</h1>:<h1>Zero</h1>};
   </div>
 );
 }
