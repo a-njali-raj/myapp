12e352a88e179ac6a26b5d5e9733f23d2971dade
Anjali Raj <anjaliraj2024a@mca.ajce.in>
2024-08-10 19:49:55 +0530

    Fetching data by API

--- src/App.js
+++ src/App.js
@@ -1,15 +1,29 @@
 
 import './App.css';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Effects from './components/Effects/Effects';
+
 
 function App() {
+  const[posts,setPosts]=useState([])
+  useEffect(()=>{
+    fetch('https://jsonplaceholder.typicode.com/posts')
+      .then(response => response.json())
+      .then(posts => setPosts(posts.splice(0,10)))//splice for getting 10 data
+  
+
+  },[])
   
   return(
  <div className='myapp'>
   
- <Effects/>
+<h1 className='bg-primary text-white'>fetch API data</h1>
+{posts.map((post)=>
+  <div key={post.id}className='bg-dark'>
+<h1 className='text-info'>{post.title}</h1>
+<p className='text-white'>{post.body}</p>
+</div>
+)}
   </div>
 );
 }
